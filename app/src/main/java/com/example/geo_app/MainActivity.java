c7731d5693e1be329625c6465d6d255ca9f1e052
ahlam <ahlam.sg7@gmail.com>
2022-01-05 22:29:17 +0300

    create activity_main.xml

---
+++ app/src/main/java/com/example/geo_app/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.geo_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickPlayBtn(View view) {
+        Intent intent = new Intent(this, Selection.class);
+        startActivity(intent);
+    }
+
+    public void onClickLeaderBoardBtn(View view) {
+    }
+
+    public void onClickSettingsBtn(View view) {
+    }
+
+    public void onClickCreditsBtn(View view) {
+    }
+
+    public void onClickLoginBtn(View view) {
+    }
+}
