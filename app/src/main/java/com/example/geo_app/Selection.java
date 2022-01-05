40348e98b0302b826547e33282a5a1b2ae24c632
ahlam <ahlam.sg7@gmail.com>
2022-01-05 23:01:14 +0300

    create activity_selection.xml

--- app/src/main/java/com/example/geo_app/Selection.java
+++ app/src/main/java/com/example/geo_app/Selection.java
@@ -2,7 +2,9 @@ package com.example.geo_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Selection extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class Selection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
+    }
+
+    public void onClickBackBtn(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickFlagBtn(View view) {
+    }
+
+    public void onClickCapitalBtn(View view) {
+    }
+
+    public void onClickMapBtn(View view) {
     }
 }
