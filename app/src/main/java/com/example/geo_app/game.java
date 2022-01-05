3af97564afa1b60feaea4b8fe9628f75a2a59dc8
ahlam <ahlam.sg7@gmail.com>
2022-01-06 00:51:30 +0300

    create activity_game.xml

---
+++ app/src/main/java/com/example/geo_app/game.java
@@ -0,0 +1,18 @@
+package com.example.geo_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+    public void onClickPauseBtn(View view) {
+    }
+}
