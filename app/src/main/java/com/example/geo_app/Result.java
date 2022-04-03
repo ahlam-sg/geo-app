4600aa2db01515ea9c15330ff348803f240da7c0
ahlam <ahlam.sg7@gmail.com>
2022-04-03 21:22:47 +0300

    fixed recycler view in Result.java

--- app/src/main/java/com/example/geo_app/Result.java
+++ app/src/main/java/com/example/geo_app/Result.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 
 public class Result extends AppCompatActivity {
 
-    TextView testTV;
     RecyclerView reviewRV;
     ArrayList<ReviewModel> reviewModel = new ArrayList<>();
     String category;
@@ -22,17 +21,19 @@ public class Result extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         reviewRV = findViewById(R.id.review_rv);
-        testTV = findViewById(R.id.test_tv);
 
         Intent intent = getIntent();
         category = intent.getStringExtra(Constants.CATEGORY_KEY);
         reviewModel = (ArrayList<ReviewModel>)intent.getSerializableExtra(Constants.REVIEW_MODEL_ARRAYLIST);
-        testTV.setText(reviewModel.get(0).getQuestion());
 
-        //________ISSUE IS HERE
-        ReviewAdapter reviewAdapter = new ReviewAdapter(this, reviewModel);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        reviewRV.setLayoutManager(linearLayoutManager);
-        reviewRV.setAdapter(reviewAdapter);
+        setReviewRV();
+    }
+
+    public void setReviewRV(){
+        reviewRV = findViewById(R.id.review_rv);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        RecyclerView.Adapter adapter = new ReviewAdapter(reviewModel);
+        reviewRV.setLayoutManager(layoutManager);
+        reviewRV.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/geo_app/ReviewAdapter.java
+++ app/src/main/java/com/example/geo_app/ReviewAdapter.java
@@ -1,43 +1,52 @@
 package com.example.geo_app;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
-public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.Viewholder> {
+public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.itemViewHolder> {
 
-    private Context context;
     private ArrayList<ReviewModel> reviewModelArrayList;
 
-    // Constructor
-    public ReviewAdapter(Context context, ArrayList<ReviewModel> reviewModelArrayList) {
-        this.context = context;
-        this.reviewModelArrayList = reviewModelArrayList;
+    class itemViewHolder extends RecyclerView.ViewHolder{
+        ImageView questionIV;
+        TextView questionTV, option1TV, option2TV, option3TV, option4TV;
+        public itemViewHolder(View itemView) {
+            super(itemView);
+            questionIV =  itemView.findViewById(R.id.question_iv);
+            questionTV = itemView.findViewById(R.id.question_tv);
+            option1TV = itemView.findViewById(R.id.option1_tv);
+            option2TV = itemView.findViewById(R.id.option2_tv);
+            option3TV = itemView.findViewById(R.id.option3_tv);
+            option4TV = itemView.findViewById(R.id.option4_tv);
+        }
     }
 
-    @NonNull
-    @Override
-    public ReviewAdapter.Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        // to inflate the layout for each item of recycler view.
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.review_card, parent, false);
-        return new Viewholder(view);
+    //constructor
+    public ReviewAdapter(ArrayList<ReviewModel> list){
+        reviewModelArrayList = list;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ReviewAdapter.Viewholder holder, int position) {
-        // to set data to textview and imageview of each card layout
-        ReviewModel model = reviewModelArrayList.get(position);
-        holder.questionTV.setText(model.getQuestion());
-        holder.option1TV.setText(model.getOption1());
-        holder.option2TV.setText(model.getOption2());
-        holder.option3TV.setText(model.getOption3());
-        holder.option4TV.setText(model.getOption4());
+    public itemViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.review_card, parent, false);
+
+        itemViewHolder itemViewHolder = new itemViewHolder(view);
+        return itemViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(itemViewHolder holder, int position) {
+        holder.questionTV.setText(reviewModelArrayList.get(position).getQuestion());
+        holder.option1TV.setText(reviewModelArrayList.get(position).getOption1());
+        holder.option2TV.setText(reviewModelArrayList.get(position).getOption2());
+        holder.option3TV.setText(reviewModelArrayList.get(position).getOption3());
+        holder.option4TV.setText(reviewModelArrayList.get(position).getOption4());
 
         //for flag or map
         holder.questionIV.setImageResource(R.drawable.lu_flag);
@@ -45,25 +54,6 @@ public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.Viewholder
 
     @Override
     public int getItemCount() {
-        // this method is used for showing number
-        // of card items in recycler view.
         return reviewModelArrayList.size();
-    }
-
-    // View holder class for initializing of
-    // your views such as TextView and Imageview.
-    public class Viewholder extends RecyclerView.ViewHolder {
-        private ImageView questionIV;
-        private TextView questionTV, option1TV, option2TV, option3TV, option4TV;
-
-        public Viewholder(@NonNull View itemView) {
-            super(itemView);
-            questionIV = itemView.findViewById(R.id.question_iv);
-            questionTV = itemView.findViewById(R.id.question_tv);
-            option1TV = itemView.findViewById(R.id.option1_tv);
-            option2TV = itemView.findViewById(R.id.option2_tv);
-            option3TV = itemView.findViewById(R.id.option3_tv);
-            option3TV = itemView.findViewById(R.id.option4_tv);
-        }
     }
 }
