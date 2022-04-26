a095284d3ee1e6f232352ba396bf0faa87c1fd2c
ahlam <ahlam.sg7@gmail.com>
2022-04-26 05:53:35 +0300

    create SignIn.java and activity_sign_in.xml

---
+++ app/src/main/java/com/example/geo_app/SignIn.java
@@ -0,0 +1,27 @@
+package com.example.geo_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+
+    public void signIn(View view) {
+    }
+
+    public void signInWithGoogle(View view) {
+    }
+
+    public void forgotPassword(View view) {
+    }
+
+    public void register(View view) {
+    }
+}
