bb732c8ff30e0034b49e8f7d9c1f10b7ac9d8133
ahlam <ahlam.sg7@gmail.com>
2022-06-18 01:35:15 +0300

    fix status bar color in SignUpActivity

--- app/src/main/java/com/example/geo_app/SignUpActivity.java
+++ app/src/main/java/com/example/geo_app/SignUpActivity.java
@@ -67,7 +67,7 @@ public class SignUpActivity extends AppCompatActivity {
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         TypedValue typedValue = new TypedValue();
-        this.getTheme().resolveAttribute(R.attr.backgroundColor, typedValue, true);
+        this.getTheme().resolveAttribute(R.attr.background, typedValue, true);
         getWindow().setStatusBarColor(typedValue.data);
     }
 
